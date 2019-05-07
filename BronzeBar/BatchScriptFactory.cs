72ab935fd34fcfe399c97feeb4e86a0180f20c1d
Ad Zeevaarders <zealord.dev@gmail.com>
2019-05-08 00:21:38 +0200

    First rough version of deployment tool BronzeBar

---
+++ BronzeBar/BatchScriptFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BronzeBar
+{
+    class BatchScriptFactory
+    {
+        private readonly string programRootDirectory;
+        public BatchScriptFactory(string workingDir)
+        {
+            programRootDirectory = workingDir;
+        }
+        public string CreateAppDeployer(string packageName, string appName, string fullDestinationPath)
+        {
+            string batchFile = File.ReadAllText(Path.Combine(programRootDirectory, "template_appdeployer.bat"));
+            batchFile = string.Format(batchFile, fullDestinationPath, appName);
+            return batchFile;
+        }
+    }
+}
---
+++ BronzeBar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BronzeBar;
+
+namespace BronzeBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BronzeBar.PrintLine("Welcome to Bronze Bar.");
+            BronzeBar.DoStartup();
+            bool exitRequested = false;
+            while (!exitRequested)
+            {
+                string userInput = BronzeBar.GetUserInput();
+                BronzeBar.ParseUserInput(userInput);
+            }
+        }
+    }
+}
